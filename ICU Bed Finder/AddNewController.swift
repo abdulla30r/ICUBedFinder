67da4d0872a57746590ad3f250ad640469254166
Abdulla Rahman <abdulla30r@gmail.com>
2023-11-24 02:15:39 -0800

    Data added successfull

--- ICU Bed Finder/AddNewController.swift
+++ ICU Bed Finder/AddNewController.swift
@@ -5,6 +5,7 @@
 //  Created by Abdulla Rahman on 23/11/23.
 //
 
+import FirebaseFirestore
 import UIKit
 
 class AddNewController: UIViewController {
@@ -12,12 +13,12 @@ class AddNewController: UIViewController {
     
     @IBOutlet weak var inputName: UITextField!
     
+    @IBOutlet weak var inputTotalBed: UITextField!
     @IBOutlet weak var inputContact: UITextField!
     @IBOutlet weak var inputPostal: UITextField!
     @IBOutlet weak var inputDistrict: UITextField!
     @IBOutlet weak var inputStreet: UITextField!
     @IBOutlet weak var inputAvailableBed: UITextField!
-    @IBOutlet weak var inputTotalBed: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         labelError.isHidden = true
@@ -34,9 +35,100 @@ class AddNewController: UIViewController {
         let postal:String? = inputPostal.text
         let street:String? = inputStreet.text
         
-        if((name!.isEmpty) || (total!.isEmpty||available!.isEmpty) || (contact!.isEmpty||postal!.isEmpty) || (district!.isEmpty||street!.isEmpty)){
-            labelError.text = "All Field Required"
-            labelError.isHidden = false
+        
+        
+        
+        if((name!.isEmpty) || (total!.isEmpty)||(available!.isEmpty) || (contact!.isEmpty)||(postal!.isEmpty) || (district!.isEmpty)||(street!.isEmpty)){
+            
+                labelError.text = "All Field Required"
+                labelError.isHidden = false
+        }
+        
+        else{
+            //if totalbed and available integer or not
+            if let totalValue = Int(total!) {
+                if(totalValue>0){
+                    if let availableValue = Int(available!) {
+                        if(availableValue>=0){
+                            if(availableValue<=totalValue){
+                                if let postalValue = Int(postal!){
+                                    if let contactValue = Int(contact!){
+                                        
+                                        
+                                        
+                                        //validation complete
+                                        //now add to firestore
+                                        let hospitalData = Hospital(name: name!, totalBed: totalValue, availableBed: availableValue, street: street!, district: district!, postalCode: postalValue, contact: contactValue)
+                                        
+                                        
+                                        print(hospitalData.dictionaryRepresentation)
+                                        
+                                        
+                                        Firestore.firestore().collection("Hospitals").addDocument(data: hospitalData.dictionaryRepresentation) {error in
+                                            
+                                            if let error = error {
+                                                
+                                                //Turjo ekhane alert dis//
+                                                //*******TURJO*******//
+                                                self.view.makeToast("Error: \(error)")
+                                            }
+                                            else{
+                                                self.view.makeToast("Data added Successfully")
+                                                //go to dashboard
+                                                //do some delay for toast show
+                                                
+                                                DispatchQueue.main.asyncAfter(deadline: .now()+1.0){
+                                                    let vc = self.storyboard?.instantiateViewController(identifier: "Dashboard") as! DashboardController
+                                                    vc.modalPresentationStyle = .fullScreen
+                                                    self.present(vc, animated: true)
+                                                }
+                                                
+                                            }
+                                        }
+                                        
+                                        //firebase add code
+                                        //
+                                        //complete
+                                        
+                                        
+                                        
+                                        
+                                        
+                                    }
+                                    else{
+                                        labelError.text = "Contact number is invalid"
+                                        labelError.isHidden = false
+                                    }
+                                }
+                                else{
+                                    labelError.text = "Postal Code is invalid"
+                                    labelError.isHidden = false
+                                }
+                                
+                            }
+                            else{
+                                labelError.text="Available bed can't be more than total bed"
+                                labelError.isHidden = false
+                            }
+                        }
+                        else{
+                            labelError.text = "Availble bed value can't be negative"
+                            labelError.isHidden = false
+                        }
+                    }
+                    else{
+                        labelError.text = "Availale Bed value must be an integer"
+                        labelError.isHidden = false
+                    }
+                }
+                else{
+                    labelError.text = "Total Bed must be greater than 0"
+                }
+            }
+            else{
+                labelError.text = "Total Bed must be an integer"
+                labelError.isHidden = false
+            }
         }
         
     }
